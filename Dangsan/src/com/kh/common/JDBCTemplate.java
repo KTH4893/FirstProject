e93cefbd2cc381af7ec7bf9d011c0133ecafe0a7
kth4893 <kth4893@gmail.com>
2019-10-17 09:59:16 +0900

    제발~ 그대로 받아주세요~

---
+++ Dangsan/src/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,106 @@
+package com.kh.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class JDBCTemplate {
+
+	public JDBCTemplate() {
+	}
+	//Connection 객체 생성 메소드
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","mvc","1234");
+			//자동으로 commit을 지원해주는 기능을 해제
+			conn.setAutoCommit(false);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+		
+	}
+	
+	//커넥션소멸
+	public static void close(Connection conn) {
+		
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//커밋용메소드
+	public static void commit(Connection conn) {
+		try {
+			if(conn !=null & !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//롤백
+	public static void rollback(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//stmt, pstmt, rset
+	public static void close(Statement stmt) {
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//PreparedStatement는 Statement를 상속한 자식이여서 위에 메소드를 다형성을 이용해서 사용할 수 있다.!
+//	public void close(PreparedStatement pstmt) {
+//			try {
+//				if(pstmt != null && !pstmt.isClosed()) {
+//					pstmt.close();
+//				}
+//			} catch (SQLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//	}
+	
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
